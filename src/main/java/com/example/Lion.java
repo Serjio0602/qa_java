da4b43d8223d8c4f9317dff407bde34237bfd16b
Serge S <iqarus1987@gmail.com>
2026-09-10 17:46:16 +0300

    Apply dependency injection to Lion class

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,8 +5,10 @@ import java.util.List;
 public class Lion {
 
     boolean hasMane;
+    private Feline feline;
 
-    public Lion(String sex) throws Exception {
+    public Lion(String sex, Feline feline) throws Exception {
+
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -14,9 +16,9 @@ public class Lion {
         } else {
             throw new Exception("Используйте допустимые значения пола животного - самей или самка");
         }
-    }
 
-    Feline feline = new Feline();
+        this.feline = feline;
+    }
 
     public int getKittens() {
         return feline.getKittens();
